3771db071845bec2fcac0095251d23ce85f523c1
nuisancedm <2019684871@qq.com>
2024-03-03 12:05:15 +0800

    .

--- c000_test.cpp
+++ c000_test.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
-int main() {
-  std::cout << "hello world!" << std::endl;
-  return 0;
+int main()
+{
+    std::cout << "hello world!" << std::endl;
+    return 0;
 }
+
+// about CPP
+// how CPP transforms the code text into a excutable binary ?
+
+    // stage 1: preprocess
+    // deal with all of the preprocess statement
+
+    // stage 2：compile
+    // we write cpp files just in text. 
